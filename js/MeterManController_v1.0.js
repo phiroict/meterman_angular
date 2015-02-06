f6e9b0259b0311686996d50bd776820988c5c924
Philip Rodrigues <philip.rodrigues@solnet.co.nz>
2015-02-07 11:13:27 +1300

    Implemented changes for upgrade to angular 1.3.12 (from 1.1.2)

--- js/MeterManController_v1.0.js
+++ js/MeterManController_v1.0.js
@@ -1,25 +1,30 @@
-var testFunc = function(){
-    alert("Clicked");
-}
+var metermanApp = angular.module('metermanApp', []);
 
-function MeterManCtrl($scope) {
-   $scope.meterFields = [
-       { text : "Meter In High", value:0, id:"meterInHigh" },
-       { text : "Meter In Low", value:0, id:"meterInLow" },
-       { text : "Meter Out High", value:0, id:"meterOutHigh" },
-       { text : "Meter In Low", value:0, id:"meterOutLow" },
-       { text : "Meter PV In", value:0, id:"meterPvIn" }
-   ]
-   $scope.buttons = [
-        {text:"Clear",id:"cmdClear",onClick:"testFunc()"},
-        {text:"Get PV",id:"cmdGetPV",onClick:"testFunc()"},
-        {text:"Submit pvoutput",id:"cmdSubmitPVOutput",onClick:"testFunc()"}
+metermanApp.controller('MeterManCtrl', function($scope){
+
+    $scope.testFunc = function(){
+        alert("Clicked");
+    }
+
+
+    $scope.meterFields = [
+        { text : "Meter In High", value:0, id:"meterInHigh" },
+        { text : "Meter In Low", value:0, id:"meterInLow" },
+        { text : "Meter Out High", value:0, id:"meterOutHigh" },
+        { text : "Meter In Low", value:0, id:"meterOutLow" },
+        { text : "Meter PV In", value:0, id:"meterPvIn" }
+    ]
+    $scope.buttons = [
+        {text:"Clear",id:"cmdClear"},
+        {text:"Get PV",id:"cmdGetPV"},
+        {text:"Submit pvoutput",id:"cmdSubmitPVOutput"}
     ]
     $scope.footers = [
-        {text:"Meter values",id:"cmdMeterValues",onClick:"testFunc()"},
-        {text:"PVOutput",id:"cmdPVOutput",onClick:"testFunc()"},
-        {text:"Settings",id:"cmdSettings",onClick:"testFunc()"}
+        {text:"Meter values",id:"cmdMeterValues"},
+        {text:"PVOutput",id:"cmdPVOutput"},
+        {text:"Settings",id:"cmdSettings"}
     ]
-};
+})
+
 
 
